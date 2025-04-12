e49b730b953c823c22bc61377a8016789957d9c9
Helder Balbino <helderbalbino@googlemail.com>
2025-04-12 13:37:03 +0100

    refactor: enhance comments for Sequelize instance creation and database configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,6 +5,11 @@ import dotenv from 'dotenv';
 // This is important to ensure that the environment variables are loaded before using them
 dotenv.config();
 
+// Create a new Sequelize instance
+// The Sequelize constructor takes the database name, username, password, and options
+// The options include the host and dialect (MySQL in this case)
+// The environment variables are accessed using process.env
+// The 'as string' assertion is used to tell TypeScript that these variables are strings
 const sequelize = new Sequelize(
 	process.env.DB_NAME as string, // Database name
 	process.env.DB_USER as string, // Database user
@@ -12,6 +17,7 @@ const sequelize = new Sequelize(
 
 	{
 		host: process.env.DB_HOST as string, // Database host
+		port: parseInt(process.env.DB_PORT as string), // Database port
 		dialect: 'mysql', // Database dialect
 	},
 );
