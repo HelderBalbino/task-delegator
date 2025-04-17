81af7db6fd7635812d13dc91bf02aad97d545e16
Helder Balbino <helderbalbino@googlemail.com>
2025-04-17 13:59:26 +0100

    refactor: update error message for user profile update to English

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -209,7 +209,7 @@ class UserController {
 
 			if (user.primary_user_id !== userId && user.id !== userId) {
 				res.status(403).json({
-					error: 'Usuário sem permissão para atualizar o perfil de outro usuário.',
+					error: 'user without permission to update another user profile.',
 				});
 				return;
 			}
