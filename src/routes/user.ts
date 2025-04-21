c3cb84532f2b95ff9ffa5a631e6ed1f394355888
Helder Balbino <helderbalbino@googlemail.com>
2025-04-21 23:22:42 +0100

    feat: add login route to user router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,5 +5,6 @@ import authenticate from '../middlewares/authMiddleware';
 const router = Router();
 
 router.post('/register', UserController.register);
+router.post('/login', UserController.login);
 
 export default router;
