d19582d1ecbdb405ecf97dc2b1cbc918e352defb
Helder Balbino <helderbalbino@googlemail.com>
2025-04-21 23:23:34 +0100

    feat: add route to get user by ID with authentication

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,5 +7,6 @@ const router = Router();
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.post('/register-user', authenticate, UserController.registerDependent);
+router.get('/:id', authenticate, UserController.getUserById);
 
 export default router;
