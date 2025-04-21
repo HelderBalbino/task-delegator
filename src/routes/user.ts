51be418b675788fd87e71c6ef2184def97c0f58e
Helder Balbino <helderbalbino@googlemail.com>
2025-04-21 23:23:05 +0100

    feat: add route for registering dependent users

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,5 +6,6 @@ const router = Router();
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
+router.post('/register-user', authenticate, UserController.registerDependent);
 
 export default router;
