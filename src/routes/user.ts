41aafc41a2c7efe533a5bfd2fb66ca60010d2262
Helder Balbino <helderbalbino@googlemail.com>
2025-04-21 23:24:42 +0100

    feat: add update user route with authentication

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,5 +8,6 @@ router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.post('/register-user', authenticate, UserController.registerDependent);
 router.get('/:id', authenticate, UserController.getUserById);
+router.put('/:id', authenticate, UserController.updateUser);
 
 export default router;
