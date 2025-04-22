24b1a874a2a6b38ad252da511ce92214cdff520a
Helder Balbino <helderbalbino@googlemail.com>
2025-04-22 14:03:42 +0100

    feat: add authentication middleware to task route

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import TaskController from '../controllers/taskController';
+import authenticate from '../middlewares/authMiddleware';
 
 const router = Router();
 
