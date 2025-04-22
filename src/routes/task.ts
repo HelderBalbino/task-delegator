25e810d9ad4165e1693f78da7ec0031ac049a2a7
Helder Balbino <helderbalbino@googlemail.com>
2025-04-22 14:06:17 +0100

    feat: add GET route for retrieving tasks by page with authentication

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -5,5 +5,6 @@ import authenticate from '../middlewares/authMiddleware';
 const router = Router();
 
 router.post('/task', authenticate, TaskController.createTask);
+router.get('/tasks/:page', authenticate, TaskController.getTasksByPage);
 
 export default router;
