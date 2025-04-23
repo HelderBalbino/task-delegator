3d783a8a5d060e48eabb0ca5e5ba834a818fa9f6
Helder Balbino <helderbalbino@googlemail.com>
2025-04-23 15:21:10 +0100

    chore: restructure app.ts to enhance server initialization and routing clarity

--- src/app.ts
+++ src/app.ts
@@ -18,3 +18,22 @@ app.use(
 		credentials: true, // Permite o envio de cookies e headers de autenticação
 	}),
 );
+
+app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+	try {
+		await sequelize.sync({ force: false }); // Força a criação das tabelas
+		console.log('Banco de dados conectado!');
+		app.listen(PORT, () =>
+			console.log(`Servidor rodando na porta ${PORT}`),
+		);
+	} catch (error) {
+		console.error('Erro ao conectar no banco de dados:', error);
+	}
+};
+
+startServer();
