d55d2339c6319767efa77ecf364bec80f916affc
Helder Balbino <helderbalbino@googlemail.com>
2025-04-23 15:20:42 +0100

    chore: reorganize app.ts for improved readability and structure

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,17 @@ import userRoutes from './routes/userRoutes';
 import taskRoutes from './routes/taskRoutes';
 import sequelize from './config/db';
 import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// cors configuration
+app.use(
+	cors({
+		origin: [/http:\/\/localhost:\d+$/], // Permite qualquer porta do localhost
+		methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
+		credentials: true, // Permite o envio de cookies e headers de autenticação
+	}),
+);
