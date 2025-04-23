c1d307f8e8f1041d1830be48cce7c731bd9aebca
Helder Balbino <helderbalbino@googlemail.com>
2025-04-23 15:52:12 +0100

    feat: add CORS middleware and related documentation for enhanced API security

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './config/db';
-import cors from 'cors';
+// import cors from 'cors';
 import userRoutes from './routes/user';
 import taskRoutes from './routes/task';
 
@@ -11,13 +11,13 @@ const app = express();
 app.use(express.json());
 
 // cors configuration
-app.use(
-	cors({
-		origin: [/http:\/\/localhost:\d+$/], //only allow localhost
-		methods: 'GET,POST,PUT,DELETE', // Allowed methods
-		credentials: true, // Allow credentials
-	}),
-);
+// app.use(
+// 	cors({
+// 		origin: [/http:\/\/localhost:\d+$/], //only allow localhost
+// 		methods: 'GET,POST,PUT,DELETE', // Allowed methods
+// 		credentials: true, // Allow credentials
+// 	}),
+// );
 
 app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
