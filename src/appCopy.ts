80ba8f69f1d577f52c1d5b47b93be289160a1edb
Helder Balbino <helderbalbino@googlemail.com>
2025-04-23 15:17:39 +0100

    feat: implement database setup and initial data creation for testing

---
+++ src/appCopy.ts
@@ -0,0 +1,25 @@
+//only for database testing purposes
+import { createDemand, createUser } from './test/test';
+import sequelize from './config/db';
+
+// Import the sequelize instance
+const createTables = async () => {
+	try {
+		// Create the tables
+		await sequelize.sync({ force: true });
+		console.log('tables were successfully created!');
+		//
+	} catch (error) {
+		// Handle any errors that occur during table creation
+		// Log the error to the console
+		console.error('Error when creating table:', error);
+	}
+};
+
+const startServer = async () => {
+	await createTables();
+	await createUser();
+	await createDemand();
+};
+
+startServer();
